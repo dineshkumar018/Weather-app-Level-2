073c6594eb260af294c7f3cba98219f197d0405b
Dinesh Kumar <dineshkumar180603@gmail.com>
2026-03-30 14:54:20 +0530

    re-fixed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Weather-app-Level-2/",
+  base: "/Weather-app-Level-2/Level%202/",
   plugins: [react()],
 })
